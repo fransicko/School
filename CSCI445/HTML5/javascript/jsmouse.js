0047b9a26ab52a812fc17192b50851beae6ce4f2
mvillafu_s Student Worker Account) <mvillafu@mymail.mines.edu>
2017-09-26 11:09:04 -0600

    Made the images switch between light and dark naaru by clicking on the canvas. Now i need the button.

--- CSCI445/HTML5/javascript/jsmouse.js
+++ CSCI445/HTML5/javascript/jsmouse.js
@@ -1,5 +1,62 @@
+// To load the image 
+var canvas, ctx, img, rect, pic;
+
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var img = new Image();
-img.src = "../images/Happy_face.jpg";
-ctx.drawImage(img, 10, 10);
+
+window.onload = function() {
+	img = new Image();
+	img.src = "../images/naaru_light.png";
+	pic = "light";
+	ctx.drawImage(img, 15, 10);
+}
+
+// reload functionf
+function change() {
+	location.reload();
+}
+
+function getMousePos(canvas, event) {
+	var pos = canvas.getBoundingClientRect();
+	return {
+		x: event.clientX - pos.left,
+		y: event.clientY - pos.top
+	};
+}
+
+// Checks to see if hte click was inside the canvas rectangle
+function insideRec(pos, rect) {
+	return (pos.x > rect.x) && (pos.x < rect.x + rect.width) && (pos.y > rect.y) && (pos.y < rect.y + rect.height);
+}
+
+// Defining the image bounderies, the width and hieght were taken from the image properties
+rect = {
+	x:15,
+	y:10,
+	width:505,
+	height:676
+};
+
+// adding the click event
+canvas.addEventListener("click", function(evt) {
+	var mousePos = getMousePos(canvas, evt);
+	
+	if (insideRec(mousePos, rect)) {
+		//alert("clicked ubsude rect");
+		if (pic === "light") {
+			img.src = "../images/naaru_dark.png";
+			//alert("light");
+			pic = "dark";
+			ctx.drawImage(img, 15, 10);
+		} else {
+			//img.src = "../images/naaru_light.png";
+			img.src = "../images/naaru_light.png";
+			//alert("dark");
+			pic = "light";
+			ctx.drawImage(img, 15, 10);
+		}
+	} else {
+		//alert("clicked outside rect");
+	}
+	
+}, false);
