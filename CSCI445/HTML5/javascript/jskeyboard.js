683572fa903165e5dff017240ae9b623846e5118
Michael Villafuerte <mvillafu@mymail.mines.edu>
2017-09-26 23:47:59 -0600

    Working on part two fixed part one.

---
+++ CSCI445/HTML5/javascript/jskeyboard.js
@@ -0,0 +1,62 @@
+// To load the image 
+var canvas, ctx, img, rect, xPos, yPos;
+
+canvas = document.getElementById("myCanvas");
+img = document.getElementById("happy_face");
+ctx = canvas.getContext("2d");
+xPos = 15;
+yPos = 10;
+
+window.onload = function() {
+	img = new Image();
+	img.src = "../images/naaru_light.png";
+	ctx.drawImage(img, xPos, yPos);
+}
+
+// defining the boundareies of the canvas
+rect = {
+	width:460,
+	height:600
+};
+
+function move() {
+	ctx.clearRect(0, 0, rect.width, rect.height);
+	ctx.drawImage(img, xPos, yPos);
+}
+
+// THis is for the arrow key movements
+window.addEventListener("keydown", function moveSomething(e) {
+    switch(e.keyCode) {
+        case 37:
+            // left key pressed
+			xPos = xPos - 5;
+			if (xPos <= 0) xPos = 0;
+			move();
+            break;
+        case 38:
+            // up key pressed
+			yPos = yPos - 5;
+			if (yPos <= 0) yPos = 0;
+			move();
+            break;
+        case 39:
+            // right key pressed
+			xPos = xPos + 5;
+			if (xPos+100 => rect.width) xPos = rect.width-100;
+			move();
+            break;
+        case 40:
+            // down key pressed
+			yPos = yPos + 5;
+			if (yPos+135 >= rect.height) yPos = rect.height-135;
+			move();
+            break;  
+    }
+}, false);
+
+//img.src = "../images/naaru_dark.png";
+		//alert("light");
+		//pic = "frown";
+		//btn.innerText = "Smile";
+		//ctx.drawImage(img, 15, 10);
+ 
--- CSCI445/HTML5/javascript/jsmouse.js
+++ CSCI445/HTML5/javascript/jsmouse.js
@@ -6,17 +6,77 @@ btn = document.getElementById("btn");
 img = document.getElementById("happy_face");
 ctx = canvas.getContext("2d");
 
+
 window.onload = function() {
 	//img = new Image();
-	img.src = "../images/naaru_light.png";
-	pic = "light";
-	btn.innerText = "Darkness!!"
-	ctx.drawImage(img, 15, 10);
+	//img.src = "../images/naaru_light.png";
+	//pic = "light";
+	btn.innerText = "frown"
+	//ctx.drawImage(img, 15, 10);
+	drawSmileFace();
 }
 
-// reload functionf
-function change() {
-	location.reload();
+// drawing the smily face
+function drawSmileFace() {
+	drawHead();
+	drawEyes();
+	drawSmile();
+}
+
+function drawFrownFace() {
+	drawHead();
+	drawEyes();
+	drawFrown();
+}
+
+function drawHead() {
+	ctx.beginPath();
+	ctx.fillStyle = "yellow";
+	// x , y, radis, start point
+	ctx.arc(115, 110, 100, 0, Math.PI*2);
+	ctx.stroke();
+	ctx.closePath();
+	ctx.fill();
+}
+
+function drawEyes() {
+	// right eye
+	ctx.beginPath();
+	ctx.fillStyle = "black";
+	// x , y, radis, start point
+	ctx.arc(70, 55, 15, 0, Math.PI*2);
+	ctx.stroke();
+	ctx.closePath();
+	ctx.fill();
+	
+	// left eye
+	ctx.beginPath();
+	ctx.fillStyle = "black";
+	// x , y, radis, start point
+	ctx.arc(160, 55, 15, 0, Math.PI*2);
+	ctx.stroke();
+	ctx.closePath();
+	ctx.fill();
+}
+
+function drawSmile() {
+	ctx.beginPath();
+	ctx.fillStyle = "black";
+	// x , y, radis, start point
+	ctx.arc(115, 105, 75, 2*Math.PI, Math.PI);
+	ctx.lineWidth = 7;
+	ctx.stroke();
+	ctx.closePath();
+}
+
+function drawFrown() {
+	ctx.beginPath();
+	ctx.fillStyle = "black";
+	// x , y, radis, start point
+	ctx.arc(115, 185, 75, 1.1*Math.PI, Math.PI*1.9);
+	ctx.lineWidth = 7;
+	ctx.stroke();
+	ctx.closePath();
 }
 
 function getMousePos(canvas, event) {
@@ -36,8 +96,8 @@ function insideRec(pos, rect) {
 rect = {
 	x:15,
 	y:10,
-	width:505,
-	height:676
+	width:215,
+	height:210
 };
 
 // adding the click event
@@ -46,19 +106,18 @@ canvas.addEventListener("click", function(evt) {
 	
 	if (insideRec(mousePos, rect)) {
 		//alert("clicked ubsude rect");
-		if (pic === "light") {
-			img.src = "../images/naaru_dark.png";
-			//alert("light");
-			pic = "dark";
-			btn.innerText = "Light!!";
-			ctx.drawImage(img, 15, 10);
+		if (pic === "smile") {
+			pic = "frown";
+			btn.innerText = "Smile";
+			//ctx.drawImage(img, 15, 10);
+			ctx.clearRect(0, 0, rect.width, rect.height);
+			drawFrownFace();
 		} else {
-			//img.src = "../images/naaru_light.png";
-			img.src = "../images/naaru_light.png";
-			//alert("dark");
-			pic = "light";
-			btn.innerText = "Darkness!!"
-			ctx.drawImage(img, 15, 10);
+			pic = "smile";
+			btn.innerText = "Frown"
+			//ctx.drawImage(img, 15, 10);
+			ctx.clearRect(0, 0, rect.width, rect.height);
+			drawSmileFace();
 		}
 	} else {
 		//alert("clicked outside rect");
@@ -68,18 +127,22 @@ canvas.addEventListener("click", function(evt) {
 
 // THis is the button listener.
 btn.addEventListener("click", function() {
-	if (pic === "light") {
-		img.src = "../images/naaru_dark.png";
+	if (pic === "smile") {
+		//img.src = "../images/naaru_dark.png";
 		//alert("light");
-		pic = "dark";
-		btn.innerText = "Light!!";
-		ctx.drawImage(img, 15, 10);
+		pic = "frown";
+		btn.innerText = "Smile";
+		//ctx.drawImage(img, 15, 10);
+		ctx.clearRect(0, 0, rect.width, rect.height);
+		drawFrownFace();
 	} else {
 		//img.src = "../images/naaru_light.png";
-		img.src = "../images/naaru_light.png";
+		//img.src = "../images/naaru_light.png";
 		//alert("dark");
-		pic = "light";
-		btn.innerText = "Darkness!!"
-		ctx.drawImage(img, 15, 10);
+		pic = "smile";
+		btn.innerText = "Frown"
+		//ctx.drawImage(img, 15, 10);
+		ctx.clearRect(0, 0, rect.width, rect.height);
+		drawSmileFace();
 	}
 });
